891fcba979aa901c1a72b9164796528e9675123b
Tural Hasanov <hasanovtural11@gmail.com>
2023-07-23 23:32:38 +0200

    remove ball

--- client/src/components/Technologies.tsx
+++ client/src/components/Technologies.tsx
@@ -1,10 +1,10 @@
-import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { fadeIn } from "../utils/motion.js";
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import { Technology } from "../models/global.model.js";
 import backendClient from "../clients.js";
+import { imageUrl } from "../utils/image_url.js";
 
 function Technologies() {
   const [technologies, setTechnologies] = useState<Technology[]>([]);
@@ -32,7 +32,15 @@ function Technologies() {
         <div className="w-28 h-28 mb-6" key={index}>
           <motion.div variants={fadeIn("", "", 0.1 * index, 0.2)}>
             <h1 className="text-center">{technology.name}</h1>
-            <BallCanvas icon={technology.icon} />
+            <div className="flex flex-wrap justify-center items-center p-2">
+              <div className="w-full">
+                <img
+                  src={imageUrl(technology.icon).url()}
+                  alt={technology.icon._id}
+                  className="shadow max-w-full h-auto align-middle border-none"
+                />
+              </div>
+            </div>
           </motion.div>
         </div>
       ))}
